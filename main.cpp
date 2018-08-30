710e6f77ff7896cfa63607c1426d82d29ad59137
Giuseppe Acito <giuseppe.acito@gmail.com>
2018-08-30 15:18:44 +0200

    Lezione 9

--- main.cpp
+++ main.cpp
@@ -120,6 +120,26 @@ bool init() {
   return true;
 }
 
+SDL_Texture* loadTexture(std::string path) {
+  SDL_Texture* newTexture = NULL;
+  SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+
+  if(loadedSurface == NULL) {
+    std::cerr << "Unable to load image: " << path.c_str();
+    std::cerr << ", SDL Error: " << SDL_GetError() << std::endl;
+    return NULL;
+  }
+
+  newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+  if (newTexture == NULL) {
+    std::cerr << "Unable to create texture from " << path.c_str();
+    std::cerr << ", SDL Error: " << SDL_GetError() << std::endl;
+  }
+
+  SDL_FreeSurface(loadedSurface);
+  return newTexture;
+}
+
 
 void close() {
   if(gRenderer != NULL) {
